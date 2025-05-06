f81358fdd46254bfcf26088912b2519be0869c66
tungvu8998 <tungvu8998@gmail.com>
2025-05-06 13:51:57 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include <iostream> // Thu vi?n chu?n cho vi?c nh?p/xu?t (ví d?: in ra màn hình)
+#include <iostream> 
 
-// Hàm main là noi chuong trình b?t d?u th?c thi
 int main() {
     // Ð?t mã c?a b?n vào dây
-    std::cout << "Xin chao The gioi!" << std::endl; // In dòng ch? "Xin chao The gioi!" ra màn hình
+    std::cout << "Xin chao The gioi!" << std::endl; 
 
-    // Tr? v? 0 d? báo hi?u chuong trình k?t thúc thành công
+
     return 0;
 }

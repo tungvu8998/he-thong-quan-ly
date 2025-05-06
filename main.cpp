c20e3e4cb01df0b9f19dc8e6d822be40082ae9ba
tungvu8998 <tungvu8998@gmail.com>
2025-05-06 13:45:41 +0700

    demo

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Thu vi?n chu?n cho vi?c nh?p/xu?t (ví d?: in ra màn hình)
+
+// Hàm main là noi chuong trình b?t d?u th?c thi
+int main() {
+    // Ð?t mã c?a b?n vào dây
+    std::cout << "Xin chao The gioi!" << std::endl; // In dòng ch? "Xin chao The gioi!" ra màn hình
+
+    // Tr? v? 0 d? báo hi?u chuong trình k?t thúc thành công
+    return 0;
+}
